84d742b9ccd5b9afeba1b54f5d9f96a23686ad60
ericthelemur <owenconnors1@gmail.com>
2023-02-08 14:10:46 +0000

    Break in video

--- src/graphics/video/video.js
+++ src/graphics/video/video.js
@@ -5,8 +5,10 @@ import { get } from 'lodash';
 import BeachBackground from '../beach/beach.js';
 import BarComponent from '../bar/bar.js';
 import { nextRuns } from '../nextRuns/nextRuns.js';
+import { BreakMultiBox, Run } from '../break/break-pages.js';
 
 import '../common.css';
+import '../break/break.css';
 import './video.css';
 
 const replicants = {
@@ -14,33 +16,10 @@ const replicants = {
   runArray: NodeCG.Replicant('runDataArray', 'nodecg-speedcontrol'),
   backgroundMode: NodeCG.Replicant('backgroundMode', 'wasd'),
   total: NodeCG.Replicant('total', 'nodecg-tiltify'),
+  challenges: NodeCG.Replicant('challenges', 'nodecg-tiltify'),
+  polls: NodeCG.Replicant('donationpolls', 'nodecg-tiltify'),
   barAnnouncementsRep: NodeCG.Replicant('barAnnouncements', 'wasd'),
 };
-
-class UpNext {
-  view(vnode) {
-    let run;
-    if (vnode.attrs.nextRuns.length === 0) {
-      run = { game: 'No More Runs!' };
-    } else {
-      run = vnode.attrs.nextRuns[0];
-    }
-
-    const details = [
-      get(run, 'category', ''),
-      get(run, 'teams[0].players', []).map(p => p.name).join(', '),
-      get(run, 'system', ''),
-      get(run, 'estimate'),
-    ].filter(e => e).join(' / ');
-
-    return m('.video-up-next-container', [
-      m('.video-up-next-label', 'Coming Up Next'),
-      m('.video-v-space'),
-      m('.video-up-next-game', run.game),
-      m('.video-up-next-details', details),
-    ])
-  }
-}
 
 class VideoScreenComponent {
   view(vnode) {
@@ -50,8 +29,20 @@ class VideoScreenComponent {
       m(BeachBackground, { backgroundModeRep: vnode.attrs.backgroundModeRep }),
       m('.graphic .overlay', [
         m('.video-component-container', [
+          m('.break-right', [
+            m('.break-right-label', 'Coming Up Next'),
+            m('.break-h-space'),
+            ((vnode.attrs.nextRuns.length === 0)
+              ? m('.break-next-run-game', 'NO RUNS!')
+              : m(Run, { run: vnode.attrs.nextRuns[0] })),
+            m(BreakMultiBox, {
+              nextRuns: vnode.attrs.nextRuns,
+              incentives: vnode.attrs.incentives,
+              polls: vnode.attrs.polls,
+              multiline_games: true,
+            }),
+          ]),
           m('.video-greenscreen'),
-          m(UpNext, { nextRuns: vnode.attrs.nextRuns }),
         ]),
       ]),
       m(BarComponent, { total: vnode.attrs.total, barAnnouncementsRep: vnode.attrs.barAnnouncementsRep }),
@@ -66,6 +57,8 @@ NodeCG.waitForReplicants(...Object.values(replicants)).then(() => {
         total: Math.floor(replicants.total.value),
         backgroundModeRep: replicants.backgroundMode,
         nextRuns: nextRuns(replicants.run.value, replicants.runArray.value),
+        incentives: replicants.challenges.value,
+        polls: replicants.polls.value,
         barAnnouncementsRep: replicants.barAnnouncementsRep,
       });
     }
